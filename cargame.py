c9ff3144d8263f02ada0ce586e1a49517a61c359
abidurrahman11 <abidurr247@gmail.com>
2023-03-25 23:09:10 +0600

    first commit

---
+++ cargame.py
@@ -0,0 +1,94 @@
+import pygame
+from pygame.locals import *
+import random
+
+# TO DO: Add sound effect, trees graphics, on-screen score
+
+pygame.init()
+
+# variables
+size = width, height = (800, 660)
+road_w = int(width / 1.6)
+roadmark_w = int(width / 80)
+right_lane = width / 2 + road_w / 4
+left_lane = width / 2 - road_w / 4
+speed = 1
+
+running = True
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("2D Car Game")
+screen.fill((60, 220, 0))
+
+#apply changes
+pygame.display.update()
+
+# load player car
+car = pygame.image.load("car.png")
+car_loc = car.get_rect()
+car_loc.center = right_lane, height * 0.8
+
+# load enemy car
+car2 = pygame.image.load("otherCar.png")
+car2_loc = car2.get_rect()
+car2_loc.center = left_lane, height * 0.2
+
+# tracking the score
+counter = 0
+
+while running:
+    # if score is greater than 5000 then move to a new level and increase the speed of enemy car
+    counter += 1
+    if counter == 5000:
+        speed += 0.16
+        counter = 0
+        print("Level Up", speed)
+    # animate enemy vehicle
+    # adding speed to change y-axis of car2_loc
+    car2_loc[1] += speed
+    # if car2 move & disappear then, changing the loaction of new car2
+    if car2_loc[1] > height:
+        # using random integer from 0 to 1 to appear car in random order
+        if random.randint(0, 1) == 0:
+            car2_loc.center = right_lane, -200
+        else:
+            car2_loc.center = left_lane, -200
+
+    # game ending logic
+    # if both car in same location in x-axis and y-axis then the will collide
+    # we subtracting 250 from y because even if the front of car touches another car they actually collide
+    if car_loc[0] == car2_loc[0] and car2_loc[1] > car_loc[1] - 250:
+        print("GAME OVER!")
+        break
+
+    # if the car get out of the road the game will be over
+    if car_loc[0] == width / 2 + road_w / 2 or car_loc[0] == width / 2 - road_w:
+        print("GAME OVER!")
+        break
+
+    # event listener
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        if event.type == KEYDOWN:
+            if event.key in [K_a, K_LEFT]:
+                car_loc = car_loc.move([-int(road_w / 2), 0])
+            if event.key in [K_d, K_RIGHT]:
+                car_loc = car_loc.move([int(road_w / 2), 0])
+
+    # darw graphics
+    # drawing the dark road on the center of green screen
+    pygame.draw.rect(screen, (50, 50, 50), (width / 2 - road_w / 2, 0, road_w, height))
+    # drawing a yellow line on the center side of road
+    pygame.draw.rect(screen, (255, 240, 60), (width / 2 - roadmark_w / 2, 0, roadmark_w, height))
+    # drawing a white line on the left side of road
+    pygame.draw.rect(screen, (255, 255, 255), (width / 2 - road_w / 2 + roadmark_w * 2, 0, roadmark_w, height))
+    # drawing a white line on the right side of road
+    pygame.draw.rect(screen, (255, 255, 255), (width / 2 + road_w / 2 - roadmark_w * 3, 0, roadmark_w, height))
+
+    # load the car on road
+    screen.blit(car, car_loc)
+    screen.blit(car2, car2_loc)
+    pygame.display.update()
+
+pygame.quit()
